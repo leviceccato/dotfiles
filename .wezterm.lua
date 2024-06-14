6380037b699eb82997f41f38c01c8efaca64a814
Levi Ceccato <levi.ceccato@gmail.com>
2024-06-14 13:38:08 +1000

    Add general styling config for WezTerm

---
+++ .wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Macchiato'
+config.colors = {
+  background = '#111111'
+}
+config.font = wezterm.font('FiraCode Nerd Font')
+config.font_size = 16
+config.line_height = 1.3
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
