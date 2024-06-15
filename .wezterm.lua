b8da20c3e528f7bf26a86a29a67f8bde394f993a
Levi Ceccato <levi.ceccato@gmail.com>
2024-06-15 11:10:57 +1000

    Add space around term, let it breathe

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,10 +11,10 @@ config.font_size = 16
 config.line_height = 1.3
 config.enable_tab_bar = false
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
 }
 
 wezterm.on('format-window-title', function()
