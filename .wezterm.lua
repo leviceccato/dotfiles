f5d101771c467b64657ee24fe511ff2bf76447a3
Levi Ceccato <levi.ceccato@gmail.com>
2024-06-15 14:45:29 +1000

    Make style more seamless

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,23 +2,24 @@ local wezterm = require('wezterm')
 
 local config = wezterm.config_builder()
 
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+config.show_tabs_in_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.window_padding = {
+  top = 10,
+  left = 10,
+  right = 10,
+  bottom = 10,
+}
 config.color_scheme = 'Catppuccin Macchiato'
+config.window_frame = {
+  active_titlebar_bg = '#24273a'
+}
 config.colors = {
   background = '#24273a'
 }
 config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 16
 config.line_height = 1.3
-config.enable_tab_bar = false
-config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 10,
-}
-
-wezterm.on('format-window-title', function()
-  return 'WezTerm'
-end)
 
 return config
